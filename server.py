dcff3cc2bc6a485271f990b605462c00cac17765
Irene Kilgannon <irene.kilgannon@gmail.com>
2025-05-16 12:31:09 +0100

    trying again with deploy to python

--- server.py
+++ server.py
@@ -1,7 +1,6 @@
 from flask import Flask, request, abort, jsonify, render_template
 from flask_cors import CORS, cross_origin
 from PatternDAO import patternDAO
-from UserDAO import userDAO
 
 app = Flask(__name__, static_url_path='', static_folder='static')
 cors = CORS(app)
