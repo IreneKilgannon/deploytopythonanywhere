29561c22bb27e4a35cca4926dca4d32c3179741f
Irene Kilgannon <irene.kilgannon@gmail.com>
2025-05-17 16:41:51 +0100

    updated config file

--- db_config.py
+++ db_config.py
@@ -1,6 +1,6 @@
 mysql = {
-    'host':"localhost",
-    'user':"root",
-    'password':"",
-    'database':"sewing_patterns"
+    'host':"IreneKilgannon2.mysql.eu.pythonanywhere-services.com",
+    'user':"IreneKilgannon2",
+    'password':"Patterns123",
+    'database':"IreneKilgannon2$sewing_patterns"
 }
