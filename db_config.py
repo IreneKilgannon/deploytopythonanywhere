7d3f30c7c2d7fad0ea91054eb18eb65cb96cff0f
Irene Kilgannon <irene.kilgannon@gmail.com>
2025-05-17 08:42:15 +0100

    trying again with pythonanywhere, new account

--- db_config.py
+++
@@ -1,6 +0,0 @@
-mysql = {
-    'host':"IreneKilgannon.mysql.eu.pythonanywhere-services.com",
-    'user':"IreneKilgannon",
-    'password':"PatternsFun123",
-    'database':"IreneKilgannon$sewing_patterns"
-}
